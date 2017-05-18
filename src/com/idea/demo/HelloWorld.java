bbbd88a4a7603d4ad4695f0dc533c6f06bba58dd
monkdmj <monkdmj@gmail.com>
2017-05-18 20:08:05 +0800

    third commit

--- src/com/idea/demo/HelloWorld.java
+++ src/com/idea/demo/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         int tmp1 = 1;
-
+        System.out.println(tmp1);
     }
 }
